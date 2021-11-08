cdfff708ea3f0dfd19a544d1df587f729470bbf0
Raimonds Grūbe <raimisgrube@gmail.com>
2021-11-08 13:46:24 +0200

    search bar functional

---
+++ src/Components/SearchBar/SearchBar.tsx
@@ -0,0 +1,12 @@
+import './SearchBar.scss';
+import { ChangeEvent, FC } from 'react';
+
+type SearchBarProps = {
+  inputHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBar:FC<SearchBarProps> = ({ inputHandler }) => (
+  <input type="text" placeholder="item..." onChange={(event) => inputHandler(event)} className="products__search-bar" />
+);
+
+export default SearchBar;
--- src/Sections/Main/ProductSection/ProductFilterCards/ProductFilterCards.tsx
+++ src/Sections/Main/ProductSection/ProductFilterCards/ProductFilterCards.tsx
@@ -1,8 +1,9 @@
 import './ProductFilterCards.scss';
-import { FC } from 'react';
+import { ChangeEvent, FC } from 'react';
 import ProductFilterByType from '../../../../Data/ProductFilterByType';
 import { ProductItems } from '../../../../Data/ProductList';
 import Button from '../../../../Components/Buttons/Button/Button';
+import SearchBar from '../../../../Components/SearchBar/SearchBar';
 
 type ProductFilterCardsProps = {
   setProducts: (value: ProductItems[]) => void;
@@ -11,21 +12,27 @@ type ProductFilterCardsProps = {
 
 const ProductFilterCards:FC<ProductFilterCardsProps> = ({ setProducts, allProducts }) => {
   const clickHandler = (value: string) => {
-    setProducts(allProducts.filter((product) => {
-      if (value === 'all') {
-        return product;
-      }
-      return product.type === value;
-    }));
+    if (value === 'all') {
+      return setProducts(allProducts);
+    }
+    return setProducts(allProducts.filter((product) => product.type === value));
+  };
+
+  const inputFilter = (value: string) => setProducts(allProducts.filter((product) => product.title.includes(value)));
+
+  const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    inputFilter(inputValue);
   };
 
   return (
-    <div className="products__filter-button-wrapper">
+    <div className="products__filter-button-container">
       {
           ProductFilterByType.map((name) => (
             <Button key={name} title={name} onClick={() => { clickHandler(name); }} />
           ))
         }
+      <SearchBar inputHandler={inputHandler} />
     </div>
   );
 };
