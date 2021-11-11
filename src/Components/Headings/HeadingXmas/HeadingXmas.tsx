f57e08b5c039ab9d69f59c9d329a8f16f548975c
Raimonds Grūbe <raimisgrube@gmail.com>
2021-11-11 12:59:39 +0200

    Xmas title created and useffect added

---
+++ src/Components/Headings/HeadingXmas/HeadingXmas.tsx
@@ -0,0 +1,29 @@
+import { FC, useEffect } from 'react';
+
+interface HeadingXmasProps {
+    color:string;
+    xmasTitlesColorList: string[];
+    setXmasTitlesColor: (value:string) => void;
+}
+
+const HeadingXmas:FC<HeadingXmasProps> = ({ color, xmasTitlesColorList, setXmasTitlesColor }) => {
+  useEffect(() => {
+    // eslint-disable-next-line no-alert
+    alert('HeadingXmas has been loaded!');
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setXmasTitlesColor(xmasTitlesColorList.filter((item) => item
+      !== color)[Math.floor(Math.random() * 3)]), 10000);
+    return () => clearTimeout(timeout);
+  });
+
+  return (
+    <h1
+      style={{ color }}
+    >
+      Merry X-mas!
+    </h1>
+  );
+};
+export default HeadingXmas;
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,12 +2,16 @@ import { useState } from 'react';
 import { cartContent } from '../Data/CartContent';
 import Header from '../Sections/Header/Header';
 import ProductSection from '../Sections/Main/ProductSection/ProductSection';
+import HeadingXmas from '../Components/Headings/HeadingXmas/HeadingXmas';
 
 const Home = () => {
   const [cart, setCart] = useState(cartContent);
+  const xmasTitlesColorList = ['green', 'blue', 'yellow', 'red'];
+  const [color, setXmasTitlesColor] = useState('green');
   return (
     <>
       <Header cart={cart} />
+      <HeadingXmas color={color} setXmasTitlesColor={setXmasTitlesColor} xmasTitlesColorList={xmasTitlesColorList} />
       <ProductSection cart={cart} setCart={setCart} />
 
     </>
